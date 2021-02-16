e4042aa800223796d2e66ff5fa300d435222f7e5
Carlos Chacin <cchacin@gmail.com>
2021-02-16 01:37:41 -0800

    Ready to start and publish

---
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -0,0 +1,23 @@
+package org.acme;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+@QuarkusTestResource(PostgresResource.class)
+public class GreetingResourceIT {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello-resteasy-reactive")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy Reactive"));
+    }
+
+}
